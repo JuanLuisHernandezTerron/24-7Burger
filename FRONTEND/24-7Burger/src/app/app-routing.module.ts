5e5995c0f0e56e9fdf65067430cc07c9912fe523
unknown <you@example.com>
2023-07-10 12:47:48 +0200

    frontend mejoras

--- FRONTEND/24-7Burger/src/app/app-routing.module.ts
+++ FRONTEND/24-7Burger/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+import { HomeBurgerComponent } from './pages/home-burger/home-burger.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component:SkeletonComponent,
+  pathMatch:'prefix',
+  children:[
+    {path: '', component: HomeBurgerComponent}
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
--- FRONTEND/24-7Burger/src/app/app.module.ts
+++ FRONTEND/24-7Burger/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+import { HomeBurgerComponent } from './pages/home-burger/home-burger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    SkeletonComponent,
+    NavigationComponent,
+    HomeBurgerComponent
   ],
   imports: [
     BrowserModule,
