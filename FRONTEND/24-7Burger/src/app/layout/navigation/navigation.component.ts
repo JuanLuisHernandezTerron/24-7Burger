afc710aae607ecf874756746412623fdaa0e4834
Oscar <alum.oalcalae.2020@iesalixar.org>
2023-07-10 14:17:43 +0200

    header mejora

--- FRONTEND/24-7Burger/src/app/layout/navigation/navigation.component.ts
+++ FRONTEND/24-7Burger/src/app/layout/navigation/navigation.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
+  constructor(private route: ActivatedRoute){
 
+  }
+  path:string = '';
+  ngOnInit() {
+    this.route.url.subscribe(segments => {
+       this.path = segments.map(segment => segment.path).join('/');
+      console.log(this.path);
+    });
+  }
+  rutaActiva(){
+    if(this.path == "" || this.path.includes("admin")){
+      return true;
+    }else{
+      return false;
+    }
+  }
 }
