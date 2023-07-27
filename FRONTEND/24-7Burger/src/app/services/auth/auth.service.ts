3396278160d2b7eb070202100d532e72bef66930
unknown <you@example.com>
2023-07-27 13:31:34 +0200

    cambiosFront

---
+++ FRONTEND/24-7Burger/src/app/services/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/enviroments/enviroments';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { tienda } from 'src/app/models/tienda';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  private URL = environment.url;
+  constructor(private route: Router, private http:HttpClient) { }
+
+  signIn(tienda :tienda){
+    return this.http.post<tienda>(this.URL+'/adminTienda/loginTienda',tienda);
+  }
+}
